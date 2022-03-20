0eb8dd7617c9d874c349b0702cfaafffb9e7ae7f
Ryan Blushke <ryan.blushke@gmail.com>
2022-03-20 19:05:48 -0400

    Sorted Vec<isize>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if let Some((program, inputs)) = args.split_first() {
+        println!("Program Name: {:?}", program);
+        println!("Here are your inputs: {:?}", inputs);
+
+        let mut numbers: Vec<isize> = inputs.iter().map(|x| x.parse().unwrap()).collect();
+        println!("Here they are as Integers: {:?}", numbers);
+
+        numbers.sort_unstable();
+        println!("Here they are as sorted Integers: {:?}", numbers);
+    }
 }
+
