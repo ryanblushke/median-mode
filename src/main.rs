1e210c4039cdfd5880b0c714cee43b06956fd3a6
Ryan Blushke <ryan.blushke@gmail.com>
2022-03-20 22:06:39 -0400

    mean and median

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use std::env;
 
+fn mean(values: &Vec<isize>) -> f32 {
+    values.iter().sum::<isize>() as f32 / values.len() as f32
+}
+
+fn median(values: &mut Vec<isize>) -> f32 {
+    values.sort_unstable();
+    let mid = values.len() / 2;
+
+    if (values.len() % 2) == 0 {
+        (values[mid-1] + values[mid]) as f32 / 2.0
+    } else {
+        values[mid] as f32
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -10,8 +25,8 @@ fn main() {
         let mut numbers: Vec<isize> = inputs.iter().map(|x| x.parse().unwrap()).collect();
         println!("Here they are as Integers: {:?}", numbers);
 
-        numbers.sort_unstable();
-        println!("Here they are as sorted Integers: {:?}", numbers);
+        println!("mean: {}", mean(&numbers));
+        println!("median: {}", median(&mut numbers));
     }
 }
 
